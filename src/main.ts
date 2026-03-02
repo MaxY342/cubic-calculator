d1d2299e438e2d37ca19796a6ddd1d8ad8dff156
MaxY342 <max.yang342@gmail.com>
2026-03-01 19:56:28 -0500

    update styling on table and fix equation

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ let thirdRoot = document.getElementById("root3-x") as HTMLDivElement;
 let pValue = document.getElementById("p-value") as HTMLDivElement;
 let qValue = document.getElementById("q-value") as HTMLDivElement;
 let discriminantValue = document.getElementById("discriminant-value") as HTMLDivElement;
+let equation = document.getElementById("equation") as HTMLHeadingElement;
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -19,17 +20,18 @@ form.addEventListener("submit", (event) => {
     const p:number = (3 * a * c - b * b) / (3 * a * a);
     const q:number = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
     const discriminant = (q * q) / 4 + (p * p * p) / 27;
-    pValue.textContent = String(p);
-    qValue.textContent = String(q);
-    discriminantValue.textContent = String(discriminant);
+    pValue.textContent = String(p.toFixed(5));
+    qValue.textContent = String(q.toFixed(5));
+    discriminantValue.textContent = String(discriminant.toFixed(5));
+    equation.textContent = `${String(a)}x\u00b3 + ${String(b)}x\u00b2 + ${String(c)}x + ${String(d)}`
     if (discriminant < 0) {
-        const theta = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-p * p * p / 27)));
+        const theta = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-(p * p * p / 27))));
         let root1 = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
-        let root2 = 2 * Math.cbrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
-        let root3 = 2 * Math.cbrt(-p / 3) * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
-        firstRoot.textContent = String(root1);
-        secondRoot.textContent = String(root2);
-        thirdRoot.textContent = String(root3);
+        let root2 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
+        let root3 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
+        firstRoot.textContent = String(root1.toFixed(2));
+        secondRoot.textContent = String(root2.toFixed(2));
+        thirdRoot.textContent = String(root3.toFixed(2));
     } else if (discriminant > 0) {
         const u = Math.cbrt(-q / 2 + Math.sqrt(discriminant));
         const v = Math.cbrt(-q / 2 - Math.sqrt(discriminant));
@@ -40,15 +42,15 @@ form.addEventListener("submit", (event) => {
     } else {
         if (p === 0 && q === 0) {
             let root = -b / (3 * a);
-            firstRoot.textContent = String(root);
-            secondRoot.textContent = String(root);
-            thirdRoot.textContent = String(root);
+            firstRoot.textContent = String(root.toFixed(2));
+            secondRoot.textContent = String(root.toFixed(2));
+            thirdRoot.textContent = String(root.toFixed(2));
         } else {
             const u = Math.cbrt(-q / 2);
             let root1 = 2 * u - b / (3 * a);
             let root2 = -u - b / (3 * a);
-            firstRoot.textContent = String(root1);
-            secondRoot.textContent = String(root2);
+            firstRoot.textContent = String(root1.toFixed(2));
+            secondRoot.textContent = String(root2.toFixed(2));
             thirdRoot.textContent = "No real root";
         }
     }
