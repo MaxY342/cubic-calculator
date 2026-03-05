d15f6637c1909b1698c1daa0e90b51dc74bcd8a1
Max <max.yang342@gmail.com>
2026-03-05 11:21:24 -0500

    fix y-axis scaling and add root points

--- src/main.ts
+++ src/main.ts
@@ -22,41 +22,53 @@ form.addEventListener("submit", (event) => {
     const p:number = (3 * a * c - b * b) / (3 * a * a);
     const q:number = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
     const discriminant = (q * q) / 4 + (p * p * p) / 27;
+    let root1: number | undefined;
+    let root2: number | undefined;
+    let root3: number | undefined;
     pValue.textContent = String(p.toFixed(5));
     qValue.textContent = String(q.toFixed(5));
     discriminantValue.textContent = String(discriminant.toFixed(5));
     equation.textContent = `${String(a)}x\u00b3 + ${String(b)}x\u00b2 + ${String(c)}x + ${String(d)}`
     if (discriminant < 0) {
         const theta = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-(p * p * p / 27))));
-        let root1 = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
-        let root2 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
-        let root3 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
+        root1 = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
+        root2 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
+        root3 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
         firstRoot.textContent = String(root1.toFixed(2));
         secondRoot.textContent = String(root2.toFixed(2));
         thirdRoot.textContent = String(root3.toFixed(2));
     } else if (discriminant > 0) {
         const u = Math.cbrt(-q / 2 + Math.sqrt(discriminant));
         const v = Math.cbrt(-q / 2 - Math.sqrt(discriminant));
-        let root = u + v - b / (3 * a);
-        firstRoot.textContent = String(root);
+        root1 = u + v - b / (3 * a);
+        firstRoot.textContent = String(root1.toFixed(2));
         secondRoot.textContent = "No real root";
         thirdRoot.textContent = "No real root";
     } else {
         if (p === 0 && q === 0) {
-            let root = -b / (3 * a);
-            firstRoot.textContent = String(root.toFixed(2));
-            secondRoot.textContent = String(root.toFixed(2));
-            thirdRoot.textContent = String(root.toFixed(2));
+            root1 = root2 = root3 = -b / (3 * a);
+            firstRoot.textContent = String(root1.toFixed(2));
+            secondRoot.textContent = String(root2.toFixed(2));
+            thirdRoot.textContent = String(root3.toFixed(2));
         } else {
             const u = Math.cbrt(-q / 2);
-            let root1 = 2 * u - b / (3 * a);
-            let root2 = -u - b / (3 * a);
+            root1 = 2 * u - b / (3 * a);
+            root2 = -u - b / (3 * a);
             firstRoot.textContent = String(root1.toFixed(2));
             secondRoot.textContent = String(root2.toFixed(2));
             thirdRoot.textContent = "No real root";
         }
     }
     drawGraph(a, b, c, d);
+    if (root1 !== undefined) {
+        drawPoint(root1, 0);
+    }
+    if (root2 !== undefined) {
+        drawPoint(root2, 0);
+    }
+    if (root3 !== undefined) {
+        drawPoint(root3, 0);   
+    }
 });
 
 for (let i = 0; i <= 26; i++) {
@@ -75,11 +87,21 @@ for (let i = 0; i <= 26; i++) {
 
 function drawGraph(a: number, b: number, c: number, d: number) {
     ctx.strokeStyle = 'rgba(255, 0, 0, 1)';
+    const unitGap: number = 20;
+    const halfWidth: number = canvas.width / 2;
+    const halfHeight: number = canvas.height / 2;
     ctx.beginPath();
     for (let x = -13; x <= 13; x += 0.1) {
         const y = a * (x * x * x) + b * (x * x) + c * x + d;
         console.log(x, y);
-        ctx.lineTo(x*20 + 260, 260 - y);
+        ctx.lineTo(x*unitGap + halfWidth, halfHeight - y*unitGap);
     }
     ctx.stroke();
+}
+
+function drawPoint(x: number, y: number) {
+    ctx.beginPath();
+    ctx.arc(x * 20 + 260, 260 - y * 20, 3, 0, 2 * Math.PI);
+    ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+    ctx.fill();
 }
